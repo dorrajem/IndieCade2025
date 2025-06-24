b9a3e35d68480767cc4c3024600f61abd51b205e
Kimizx3 <55560064+Kimizx3@users.noreply.github.com>
2025-06-23 17:09:18 -0700

    split decks

--- Assets/__Scripts__/Individual/Kimi/Scripts/DeckRuntime.cs
+++ Assets/__Scripts__/Individual/Kimi/Scripts/DeckRuntime.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Hand : MonoBehaviour
+public class DeckRuntime : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
