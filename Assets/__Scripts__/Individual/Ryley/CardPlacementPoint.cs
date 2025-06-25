fb312508972f3d21fd700453476e2fe5ed079d0e
RiceFiend <madsaxman739@gmail.com>
2025-06-25 16:56:33 -0700

    Unfinished Colliders
    am dum

---
+++ Assets/__Scripts__/Individual/Ryley/CardPlacementPoint.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CardPlacementPoint : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public CardDisplay ActiveCard;
+    public bool isPlayerPoint;
+}
---
+++ Assets/__Scripts__/Individual/Ryley/DropArea.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropArea : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDropHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+       // Debug.Log("OnPointerEnter");
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerexit");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+        //CardDragHandler d = eventData.pointerDrag.GetComponent<CardDragHandler>();
+        //   if (d != null)
+        //    {
+        //        d.originalParent = transform;
+        //    }
+    }
+    
+
+}
