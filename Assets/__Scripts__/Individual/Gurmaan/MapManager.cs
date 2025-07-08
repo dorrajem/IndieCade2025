b8c9685ef693d57d9918fc507b9d38fbc0b32851
Gurujimaan <gabrugurmaan@gmail.com>
2025-07-08 22:49:05 +0530

    catlin scene fix

---
+++ Assets/__Scripts__/Individual/Gurmaan/MapManager.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public List<GameObject> MapEvents;
+    public List<GameObject> MapAreas;
+    public List<GameObject> MapFights;
+    
+    public List<GameObject> RandomEvents;
+    
+    void Update()
+    {
+        foreach (GameObject mapEvent in MapEvents)
+        {
+            
+        }
+    }
+}
