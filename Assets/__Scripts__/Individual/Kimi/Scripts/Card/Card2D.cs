7ba3e70341a1d785e69c4916aef2a5ba57a0c5ea
Gurujimaan <gabrugurmaan@gmail.com>
2025-07-15 22:00:57 +0530

    dynamic hearts on cards to show health

--- Assets/__Scripts__/Individual/Kimi/Scripts/Card/Card2D.cs
+++ Assets/__Scripts__/Individual/Kimi/Scripts/Card/Card2D.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 public class Card2D : MonoBehaviour, ICardVisual
 {
     public SpriteRenderer Renderer;
+    public SpriteRenderer HeartSprite;
+    public Sprite[] HeartSprites;
+    public Sprite[] SaplingSprites;
+    public Sprite[] SacrificeSprites;
     
     private SacrificeManager SacrificeManager;
+    private CardCombat CardCombat;
     void Start()
     {
         SacrificeManager = GameObject.FindWithTag("Manager").GetComponent<SacrificeManager>();
+        CardCombat=GetComponent<CardCombat>();
     }
     private void OnMouseDown()
     {
         if (SacrificeManager.Sacrificing && !SacrificeManager.CanPlace && !SacrificeManager.sacrifices.Contains(this.gameObject))
         {
             SacrificeManager.sacrifices.Add(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (CardCombat.currentHP == 0)
+        {
+            HeartSprite.sprite = null;
+        }
+
+        for (int i = 0; i < CardCombat.currentHP; i++)
+        {
+            HeartSprite.sprite = HeartSprites[i];
         }
     }
 
